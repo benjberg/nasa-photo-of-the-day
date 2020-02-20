52cc750808f04123f568dc077735f8a6031b7ad1
Ben <bbjberg91@gmail.com>
2020-02-20 17:10:26 -0500

    refactored to react/bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-const Header = ()=>{
-    return(
-        <h1>NASA PHOTO OF THE DAY</h1>
-    )
-}
+import React, {useState} from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
-
-
-
+const Header = (props) => {
+    const [collapsed, setCollapsed] = useState(true);
+  
+    const toggleNavbar = () => setCollapsed(!collapsed);
+  
+    return (
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="https://www.nasa.gov/" className="mr-auto"> <img src='C:\Users\bbjbe\Desktop\Lambda\nasa-photo-of-the-day\assets\NASA_Logo_Plaque.jpg' alt='NASA logo'/> NASA Photo Of The Day</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 
 export default Header;
+
+
+
+
+
+
+
+
--- src/components/Imagebox.jsx
+++ src/components/Imagebox.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import styled from 'styled-components';
 
+const CBody = styled(CardBody)`
+border: black 1px solid;
 
-
+`;
 const Image_box = (props) =>{
     
     return(
         <div>
-             <img alt='outerspace' src={props.img} /> 
-            <p>Title: {props.title}</p>
-            <p>Date: {props.date}</p>
-            <p>Description: {props.explanation}</p>
+             <Card>
+        <CardImg top width="100%" src={props.img} alt="Outerspace" />
+        <CBody >
+          <CardTitle>Title: {props.title}</CardTitle>
+          <CardSubtitle>Date: {props.date}</CardSubtitle>
+          <CardText>Description: {props.explanation}</CardText>
+          <Button>Button</Button>
+        </CBody>
+      </Card>
         </div>
     )
     
